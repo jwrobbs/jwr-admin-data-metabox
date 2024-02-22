b4b173a303c783ca2e332d9a48b76445f8acffc8
Josh Robbs <josh@joshrobbs.com>
2024-02-22 09:22:37 -0500

    Require/enqueue files for debugging toggle

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
  * Creates a WP admin metabox containing:
  *  - Server data
  *  - Debug data
+ *  - Debug toggle
  * Removes many default WP dashboard widgets.
  *
  * @author Josh Robbs <josh@joshrobbs.com>
@@ -22,5 +23,27 @@ namespace JWR_Admin_Metabox;
 defined( 'ABSPATH' ) || die();
 
 require_once 'php/admin-metabox-cleanup.php';
-require_once 'php/server-metabox.php';
+require_once 'php/ajax-handler.php';
 require_once 'php/git-metabox.php';
+require_once 'php/server-metabox.php';
+
+// Remove after setting up autoloder.
+require_once 'php/class-debugging-mode.php';
+
+/**
+ * Enqueue admin scripts.
+ *
+ * @return void
+ */
+function enqueue_admin_metabox_scripts() {
+	$file = plugin_dir_url( __FILE__ ) . 'js/debug-toggle.js';
+	wp_enqueue_script(
+		'jwr-admin-metabox',
+		$file,
+		array(),
+		filemtime( $file ),
+		true
+	);
+}
+
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_metabox_scripts' );
